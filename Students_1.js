815ff9c62b2da3f6cb89cb7dc754878f76850527
Simon Roberts <simon@dancingcloudservices.com>
2017-08-17 15:42:05 -0400

    More functional behaviors, parameterized criterion factory and illustration of the "this" bug in functions nested inside instance functions, along with "self = this" workaround

--- Students_1.js
+++ Students_1.js
@@ -6,6 +6,13 @@ let student = {
         return "Student: name is " + this.name
                 + " gpa is " + this.gpa
                 + " courses " + this.courses;
+    },
+    smarterThanMe : function() {
+        // patch around "bug" in design of JavaScript
+        let self = this;
+        return function(s) {
+            return s.gpa > self.gpa;
+        };
     }
 };
 
@@ -22,8 +29,15 @@ Student.orderByName = function (s1, s2) {
 Student.isEnthusiastic = function (s) {
     return s.courses.length > 2;
 };
-Student.isSmart = function(s) {
+Student.isSmart = function (s) {
     return s.gpa > 3.4;
+};
+Student.smarterThan = function (threshold) {
+    console.log("in smarterThan factory");
+    return function (s) {
+        console.log("in the test, threshold is " + threshold);
+        return s.gpa > threshold;
+    };
 };
 
 let fred = new Student("Fred", 3.2, "Math", "Physics");
@@ -50,23 +64,68 @@ function getStudents(students, criterion) {
 console.log("----------------------------");
 
 function inverse(f) {
-    return function(x) {
+    return function (x) {
         return !f(x);
     };
 }
 
-for (let s of getStudents(roster, Student.isSmart)) {
-    console.log(""+ s);
+function and(f1, f2) {
+    return function (x) {
+        return f1(x) && f2(x);
+    }
 }
+
+function or(f1, f2) {
+    return function (x) {
+        return f1(x) || f2(x);
+    }
+}
+
+//for (let s of getStudents(roster, Student.isSmart)) {
+//    console.log("" + s);
+//}
+//console.log("----------------------------");
+//
+//for (let s of getStudents(roster, Student.isEnthusiastic)) {
+//    console.log("" + s);
+//}
+//inverse(Student.isEnthusiastic)
+//console.log("----------------------------");
+//
+//for (let s of getStudents(roster, inverse(Student.isEnthusiastic))) {
+//    console.log("" + s);
+//}
+//
+//let text = [
+//    "short", "quite a bit longer", "Starts with a capital",
+//    "stuff", "fairly long"
+//];
+//
+//console.log("----------------------------");
+//
+//function long(s) {
+//    return s.length > 6;
+//}
+//
+//function reallyLong(s) {
+//    return s.length > 12;
+//}
+//
+//console.log("" + text.filter(and(long, inverse(reallyLong))));
+//
+
+let smartnessFunction = Student.smarterThan(3.4);
+console.log("about to filter students");
+console.log("" + roster.filter(smartnessFunction));
+
+console.log("----------------------------");
+smartnessFunction = Student.smarterThan(3.7);
+console.log("about to filter students again");
+console.log("" + roster.filter(smartnessFunction));
 console.log("----------------------------");
 
-for (let s of getStudents(roster, Student.isEnthusiastic)) {
-    console.log(""+ s);
-}
-inverse(Student.isEnthusiastic)
+smartnessFunction = jim.smarterThanMe();
+console.log("about to filter students again");
+console.log("" + roster.filter(smartnessFunction));
 console.log("----------------------------");
-
-for (let s of getStudents(roster, inverse(Student.isEnthusiastic))) {
-    console.log(""+ s);
-}
 
