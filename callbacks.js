978a3bee36310b24652ac13f7733eb7a609ed059
Simon Roberts <simon@dancingcloudservices.com>
2017-08-18 14:53:05 -0400

    Added callback example solution added callback example in html version

--- callbacks.js
+++ callbacks.js
@@ -10,3 +10,11 @@ console.log("1");
 setTimeout(sayHello, 1000);
 
 console.log("2");
+
+setTimeout(() => {
+    let delay = Math.random() * 2000 + 2000;
+    console.log("Starting second delay, waiting for: " + delay);
+    setTimeout(() => {
+        console.log("Second delay expired");
+    }, delay);
+}, 1000);
