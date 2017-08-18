1a23d9c6cfa68fe71509e7ef715f55fa50d1786a
Simon Roberts <simon@dancingcloudservices.com>
2017-08-18 11:05:05 -0400

    Trivial node module example

---
+++ iife.js
@@ -0,0 +1,9 @@
+
+(function () {
+    let x = 99;
+    console.log("Hello " + x);
+    function showVal(v) {
+        console.log(v);
+    }
+    showVal(x);
+})();
---
+++ useNodeModule.js
@@ -0,0 +1,6 @@
+
+let xxx = require("stuff").get();
+//console.log("thingy is " + thingy);
+xxx.doStuff();
+console.log("stuff.showhidden() gives " 
+        + xxx.showhidden());
